11612227008f185117b0f6cfca72fb65b3c0fa53
jerryxguo <jerryxguo@users.noreply.github.com>
2021-03-11 20:47:15 +1100

    implement getArnieQuotes

--- src/get-arnie-quotes.js
+++ src/get-arnie-quotes.js
@@ -2,7 +2,26 @@ const { httpGet } = require('./mock-http-interface');
 
 const getArnieQuotes = async (urls) => {
   // TODO: Implement this function.
-  // return results;
+  const getArnieQuote = async(url) => {
+    const resp = await httpGet(url);
+    try {
+      const body = JSON.parse(resp.body);
+      if(resp.status === 200){
+        return {
+          'Arnie Quote': body?.message,
+        };
+      } 
+      return {
+        FAILURE: body?.message,
+      };
+    } catch(e) {
+      return {
+        FAILURE: e.message,
+      };
+    }
+  }
+  const getQuoteCalls = urls.map(url => getArnieQuote(url));
+  return Promise.all(getQuoteCalls);
 };
 
 module.exports = {
